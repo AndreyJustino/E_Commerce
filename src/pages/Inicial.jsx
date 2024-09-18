7353fd7bd9b0614e703e1cdd8d347d2530fbb0e5
AndreyJustino <andreyjustino2020@gmail.com>
2024-09-18 18:58:46 -0300

    add loading/overlay em pag inicial

--- src/pages/Inicial.jsx
+++ src/pages/Inicial.jsx
@@ -3,20 +3,35 @@ import Header from '../components/Header'
 import style from "./Inicial.module.css"
 import CartProduct from '../components/CartProduct'
 import axios from "axios"
+import Loading from '../components/Loading'
 
 function Inicial() {
   const [data, setData] = useState([])
-
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    const overlay = document.getElementById("overlay")
+    overlay.style.display = "block"
+    
+    setLoading(true)
+    
     axios.get("https://api-e-commerce-m17f.onrender.com/getAllProducts")
     .then((response) => {
+      
+      setLoading(false)
+      overlay.style.display = "none"
+      
       setData(response.data.data)
     })
   }, [])
 
   return (
     <>
+      <div className="overlayCL" id="overlay"></div>
+      {
+        loading &&
+        <div className="loadingCL"><Loading/></div>
+      }
       <Header/>
       <main className={style.mainInicial}>
           <section className={style.sectionInicial}>
