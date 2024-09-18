23d32807e24ec59fd8c1095d2b680dca5ec3a9da
AndreyJustino <andreyjustino2020@gmail.com>
2024-09-18 16:28:04 -0300

    fazendo requisição na api para pegar dados de produtos

--- src/pages/Inicial.jsx
+++ src/pages/Inicial.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import style from "./Inicial.module.css"
 import CartProduct from '../components/CartProduct'
+import axios from "axios"
 
 function Inicial() {
+  const [data, setData] = useState([])
+
+
+  useEffect(() => {
+    axios.get("https://api-e-commerce-m17f.onrender.com/getAllProducts")
+    .then((response) => {
+      setData(response.data.data)
+    })
+  }, [])
+
   return (
-    <main className={style.mainInicial}>
-        <Header/>
-        <section className={style.sectionInicial}>
-          <CartProduct/>
-          <CartProduct/>
-          <CartProduct/>
-          <CartProduct/>
-          <CartProduct/>
-          <CartProduct/>
-          <CartProduct/>
-          <CartProduct/>
-          <CartProduct/>
-          <CartProduct/>
-        </section>
-    </main>
+    <>
+      <Header/>
+      <main className={style.mainInicial}>
+          <section className={style.sectionInicial}>
+            {
+              data.map((item, index) => {
+                return <CartProduct key={index} nomeProduto={item.nome} precoProduto={item.preco} quantidadeProduto={item.quantidade} codProduto={item.code}/>
+              })
+            }
+          </section>
+      </main>
+
+    </>
   )
 }
 
