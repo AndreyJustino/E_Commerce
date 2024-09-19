4386d0a05371fa8c15704adb5da95e316a478632
AndreyJustino <andreyjustino2020@gmail.com>
2024-09-19 14:14:58 -0300

    add react-router-dom e pag de erro

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,42 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./index.css"
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Inicial from './pages/Inicial.jsx'
+import Cadastro from './pages/Cadastro.jsx'
+import Login from './pages/Login.jsx'
+import ErrorPage from './pages/ErrorPage.jsx'
+import Carrinho from './pages/Carrinho.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: "/",
+        element: <Inicial/>
+      },
+      {
+        path: "/cadastrar",
+        element: <Cadastro/>
+      },
+      {
+        path: "/login",
+        element: <Login/>
+      },
+      {
+        path: "/carrinho",
+        element: <Carrinho/>
+      },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
+
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ErrorPage() {
+  return (
+    <p>Error 404!</p>
+  )
+}
+
+export default ErrorPage
