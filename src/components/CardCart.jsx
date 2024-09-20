1545c9b249508d1d75851eabc0fc6836bd614b45
AndreyJustino <andreyjustino2020@gmail.com>
2024-09-19 23:11:42 -0300

    componente de para cards do cart

---
+++ src/components/CardCart.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect } from 'react'
+import style from "./CardCart.module.css"
+
+function CardCart({nomeProduto, codigo, quantidade, preco}) {
+
+    useEffect(() => {
+        preco = preco * quantidade
+    }, [])
+  
+    return ( 
+
+        <div className={style.cardCart}>
+            
+            <div className={style.molduraImgCart}>
+
+            </div> {/*imagem do produto */}
+            
+            <div className={style.containerTituloCart}>
+                    <h1>nome do produto</h1>
+                    <p>codigo</p>
+                </div>
+                <div  className={style.containerPrecoCart}>
+                    <div>
+                        <button onClick={() => {
+                            let input = document.getElementById("inputQuantidade")
+
+                            let valor = Number(input.value) - 1
+                            
+                            if(valor > 0){
+                                input.value = valor
+    
+                                preco = preco * valor
+                            }
+
+
+                        }}>-</button>
+                        <input type="text" id='inputQuantidade' readOnly value="1"/>
+                        <span>
+                            <button onClick={() => {
+                                let input = document.getElementById("inputQuantidade")
+
+                                let valor = Number(input.value) + 1
+                                
+                                if(valor <= quantidade){
+                                    input.value = valor
+    
+                                    preco = preco * valor
+                                }
+
+                            }}>+</button>
+                        </span>
+                    </div>
+                    <h2>R$ {preco}</h2>
+                </div>
+
+
+            <div className={style.divContainerCart}>
+                
+            </div>
+        
+        </div>
+
+  )
+}
+
+export default CardCart
